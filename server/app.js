ff7bb22f644bbe0226c3011d03e5e66c9d458a91
Timur <makarov-timyr@yandex.ru>
2021-06-02 13:19:23 +0300

    Task_u3

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import userRoutes from './routes/userRoutes.js'
 import contactRoutes from './routes/contactRoutes.js'
 import cookieParser from 'cookie-parser'
 import * as path from 'path'
+import * as url from 'url'
 
 const app = express()
 
@@ -11,15 +12,16 @@ app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
-
 app.use('/api', userRoutes)
 app.use('/api/contact', contactRoutes)
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(path.dirname, '..', '/client/build')))
+  const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  
+  app.use(express.static(path.join(__dirname, '..', '/client/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(path.dirname, '..', 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
   )
 }
 
